1f1c5e260eb4be36a32470553d49bb202067ac61
Cristian <cris18002011@hotmail.com>
2022-06-09 14:28:12 -0500

    Creation of routes ⤴ ↘ ↩ ↗

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from '../containers/Layout'
-import Login from '../pages/Login'
-import RecoveryPwd from '../pages/RecoveryPwd'
 import Home from '../pages/Home'
+import Login from '../pages/Login'
+import RecoverPwd from '../pages/RecoverPwd'
+import SendEmail from '../pages/SendEmail'
+import NewPwd from '../pages/NewPwd'
+import MyAccount from '../pages/MyAccount'
+import CreateAccount from '../pages/CreateAccount'
+import MyOrder from '../pages/MyOrder'
+import MyOrders from '../pages/MyOrders'
 import NotFound from '../pages/NotFound'
 import '../styles/global.css'
 
@@ -14,7 +19,13 @@ const App = () => {
         <Routes>
           <Route path = '/' element = { <Home/> } />
           <Route path = '/login' element = { <Login/> } />
-          <Route path = '/recoveryPwd' element = { <RecoveryPwd/> } />
+          <Route path = '/recovery-pwd' element = { <RecoverPwd/> } />
+          <Route path = '/send-email' element = { <SendEmail/> } />
+          <Route path = '/new-password' element = { <NewPwd/> } />
+          <Route path = '/account' element = { <MyAccount/> } />
+          <Route path = '/singup' element = { <CreateAccount/> } />
+          <Route path = '/myorder' element = { <MyOrder/> } />
+          <Route path = '/myorders' element = { <MyOrders/> } />
           <Route path = '*' element = { <NotFound/> } />
         </Routes>
       </Layout>
