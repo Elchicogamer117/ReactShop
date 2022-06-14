7b10361c0c6cfbbb16db28e92e941900f71fc9fd
Cristian <cris18002011@hotmail.com>
2022-06-14 11:41:04 -0500

    Hook creation for API consumption 📦

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,28 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import ProductoItem from '@components/ProductoItem'
+import useGetProducts from '../hooks/useGetProducts'
 import '@styles/containers/ProductList.scss'
-import axios from 'axios';
 
-const API = 'https://api.escuelajs.co/api/v1/products';
+const API = 'https://api.escuelajs.co/api/v1/products'
 
 const ProductList = () => {
-  const [products, setProducts] = useState([])
-
-  //*! Warning: useEffect must not return anything besides a function
-  // useEffect(async() => {
-  //   const response = await axios.get(API)
-  //   setProducts(response.data)
-  // },[])
-  //*!It looks like you wrote useEffect(async () => ...) or returned a Promise. Instead, write the async function inside your effect and call it immediately:
-  useEffect(() => {
-    const getData = async () => {
-      const response = await axios.get(API)
-      setProducts(response.data)
-      console.log(response)
-    }
-    getData()
-    }, [])
-    
+  const products = useGetProducts(API)
   return (
     <section>
       <div className="productList">
