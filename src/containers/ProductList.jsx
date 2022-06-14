8cb6432d1c544bc8b2c4ab97428a63a3346d962a
Cristian <cris18002011@hotmail.com>
2022-06-14 11:25:24 -0500

    API consumption with useEffect 🔡

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductoItem from '@components/ProductoItem'
 import '@styles/containers/ProductList.scss'
+import axios from 'axios';
+
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+  const [products, setProducts] = useState([])
+
+  //*! Warning: useEffect must not return anything besides a function
+  // useEffect(async() => {
+  //   const response = await axios.get(API)
+  //   setProducts(response.data)
+  // },[])
+  //*!It looks like you wrote useEffect(async () => ...) or returned a Promise. Instead, write the async function inside your effect and call it immediately:
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(API)
+      setProducts(response.data)
+      console.log(response)
+    }
+    getData()
+    }, [])
+    
   return (
     <section>
       <div className="productList">
-        <ProductoItem />
+        {products.map(() => {
+        return <ProductoItem />
+        })}
       </div>
     </section>
 
